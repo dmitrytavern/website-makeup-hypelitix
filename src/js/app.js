67d68572318cee349ce47fb490e2673adf340950
dogger-work <dogger.work@gmail.com>
2020-11-08 19:12:20 +0200

    Update > added header menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+require('./parts/header')
 // require('./pages/index')
 
 console.log('[APP]: App is work')
---
+++ src/js/parts/header.js
@@ -0,0 +1,27 @@
+const $header         = $('header')
+const $headerMenuBtn  = $('#header-menu-btn')
+const $menu           = $('.menu')
+
+function scrollPage() {
+	const headerHeight = $header.outerHeight(true)
+
+	if (window.innerWidth < 992) $menu.css('padding-top', headerHeight)
+	if (window.pageYOffset > 0) {
+		$header.addClass('is-scroll')
+	} else {
+		$header.removeClass('is-scroll')
+	}
+}
+
+function toggleHeaderMenu() {
+	$headerMenuBtn.toggleClass('is-active')
+	$header.toggleClass('is-menu-open')
+	$menu.toggleClass('is-open')
+}
+
+console.log($headerMenuBtn)
+$($headerMenuBtn).on('click', toggleHeaderMenu)
+
+$(window).on('resize', scrollPage)
+$(window).on('scroll', scrollPage)
+scrollPage()
