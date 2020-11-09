8bd0999c86dee84dca79fa0faddb7bac7da28635
dogger-work <dogger.work@gmail.com>
2020-11-09 12:50:29 +0200

    Update > fixed little bugs

--- src/js/parts/modal.js
+++ src/js/parts/modal.js
@@ -11,6 +11,10 @@ function activateError(name) {
 	$(`#modal-access form input[name="${name}"]`).addClass('is-error')
 }
 
+$('#modal-access form input').on('focus', function () {
+	$(this).removeClass('is-error')
+})
+
 $('#modal-access form').submit(function (e) {
 	e.preventDefault()
 
