11a19217d370149b4f1578c2430aaa9548793928
dogger-work <dogger.work@gmail.com>
2020-11-13 20:13:47 +0200

    1.2v - fixed bugs

--- src/js/parts/header.js
+++ src/js/parts/header.js
@@ -1,16 +1,19 @@
 const $header         = $('header')
 const $headerMenuBtn  = $('#header-menu-btn')
 const $menu           = $('.menu')
-
+$header.on('resize', function () {
+	console.log('HE')
+})
 function scrollPage() {
-	const headerHeight = $header.outerHeight(true)
-
-	if (window.innerWidth < 992) $menu.css('padding-top', headerHeight)
 	if (window.pageYOffset > 0) {
 		$header.addClass('is-scroll')
 	} else {
 		$header.removeClass('is-scroll')
 	}
+
+	setTimeout(function () {
+		if (window.innerWidth < 992) $menu.css('padding-top', $header.outerHeight(true))
+	}, 300)
 }
 
 function toggleHeaderMenu() {
